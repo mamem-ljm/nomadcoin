c1d7133098ab67f95a98a6762636d88553935139
leejmin60 <leejmin60@gmail.com>
2022-08-09 15:31:44 +0900

    Revert "Revert "노마드 코인 학습 첫 커밋""
    This reverts commit 7c8c9cdd9aa53d6126300cf38da23b63c37896d7.

---
+++ blockchain/block.go
@@ -0,0 +1,73 @@
+package blockchain
+
+import (
+	"errors"
+	"fmt"
+	"nomadcoin/db"
+	"nomadcoin/utils"
+	"strings"
+	"time"
+)
+
+type Block struct {
+	Hash         string `json:"hash"`
+	PrevHash     string `json:"prevHash,omitempty"`
+	Height       int    `json:"height"`
+	Difficulty   int    `json:"difficulty"`
+	Nonce        int    `json:"nonce"`
+	Timestamp    int    `json:"timestamp"`
+	Transactions []*Tx  `json:"Transactions"`
+}
+
+var ErrNotFound = errors.New("block not found")
+
+func (b *Block) persist() {
+	db.SaveBlock(b.Hash, utils.ToBytes(b))
+
+}
+
+func (b *Block) restore(data []byte) {
+	utils.FromBytes(b, data)
+}
+
+func FindBlock(hash string) (*Block, error) {
+	blockBytes := db.Block(hash)
+	if blockBytes == nil {
+		return nil, ErrNotFound
+	}
+
+	block := &Block{}
+	block.restore(blockBytes)
+	return block, nil
+}
+
+// https://github.com/mamem-ljm/nomadcoin.git
+
+func (b *Block) mine() {
+	target := strings.Repeat("0", b.Difficulty)
+	for {
+		b.Timestamp = int(time.Now().Unix())
+		hash := utils.Hash(b)
+		fmt.Printf("Target:%s\nHash:%s\nNonce:%d\n\n\n", target, hash, b.Nonce)
+		if strings.HasPrefix(hash, target) {
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+}
+
+func createBlock(prevHash string, height int) *Block {
+	block := &Block{
+		Hash:       "",
+		PrevHash:   prevHash,
+		Height:     height,
+		Difficulty: difficulty(Blockchain()),
+		Nonce:      0,
+	}
+	block.mine()
+	block.Transactions = Mempool.txToConfirm()
+	block.persist()
+	return block
+}
